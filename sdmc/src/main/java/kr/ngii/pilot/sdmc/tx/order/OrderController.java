f84b25bc367c25cd63cadd3981d463afe9fb4f1e
sgshs <sgshs@mgaia_sgshs>
2016-08-05 18:22:02 +0900

    warning 수정
    Signed-off-by: sgshs <sgshs@mgaia_sgshs>

--- sdmc/src/main/java/kr/ngii/pilot/sdmc/tx/order/OrderController.java
+++ sdmc/src/main/java/kr/ngii/pilot/sdmc/tx/order/OrderController.java
@@ -14,7 +14,6 @@ import java.util.Locale;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
